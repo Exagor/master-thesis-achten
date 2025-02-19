7db7d60be9c467c3a8267e934e21c013850987fd
Exagor <alexandre.achten@gmail.com>
2025-02-19 17:58:46 +0100

    Right dataframe structure to excel with parser finish

--- src/pdf_parser.py
+++ src/pdf_parser.py
@@ -3,31 +3,36 @@ Simple implementation using pdf parser and a rule based approach to extract keys
 """
 
 import camelot
-import fitz  # PyMuPDF
+import fitz
 import logging
+import numpy as np
 import os
 import pandas as pd
+import pdfplumber
 import re
 
 # Create a logger
 logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
 
-def extract_with_pymupdf(pdf_path:str) -> dict:
-    # Open the PDF file
-    try:
-        document = fitz.open(pdf_path)
-    except Exception as e:
-        logging.error(f"Failed to open PDF file: {e}")
-        return {}
-    text = ""
-
-    # Iterate through each page
-    for page_num in range(len(document)):
-        page = document.load_page(page_num)
-        text += page.get_text()
+def extract_with_pdfplumber(pdf_path:str) -> str:
+    text = []
+    with pdfplumber.open(pdf_path) as pdf:
+        for page in pdf.pages:
+            text.append(page.extract_text())
+    text = "\n".join(text)
     logging.info("Text extracted from the PDF file")
+    return text
 
-    # Extract the information next to the following keywords
+def extract_with_pymupdf(pdf_path:str) -> str:
+    text = []
+    with fitz.open(pdf_path) as pdf:
+        for page in pdf:
+            text.append(page.get_text())
+    text = "\n".join(text)
+    logging.info("Text extracted from the PDF file")
+    return text
+
+def format_metadata(text:str) -> dict:
     examen_info = re.findall(r'EXAMEN\s*:\s*(\S+)', text)
     prelev_info = re.findall(r'N° du prélèvement\s*:\s*(\S+)', text)
     zone_info = re.findall(r'Origine du prélèvement\s*:\s*((?:(?!  ).)+)', text)
@@ -54,29 +59,85 @@ def extract_with_pymupdf(pdf_path:str) -> dict:
 
 def extract_tables_camelot(pdf_path:str, filename:str, save_folder_path:str):
     tables = camelot.read_pdf(pdf_path, flavor='lattice', pages='all')
-    base_filename = os.path.splitext(filename)[0]
-    tables.export(f'{save_folder_path}/tables_{base_filename}.csv', f='csv')
+    # base_filename = os.path.splitext(filename)[0]
+    # tables.export(f'{save_folder_path}/tables_{base_filename}.csv', f='csv')
     logging.info(f"Tables extracted")
     return tables
 
-def clean_dataframe():
-    pass
+def post_process_data(metadata:dict, tables):
+    # Process the metadata
+    df_doc = pd.DataFrame(metadata)
+    # Merge the two columns named "% de cellules tumorales" and "% de cellules à analyser"
+    df_doc["% de cellules"] = df_doc["% de cellules tumorales"] + df_doc["% de cellules à analyser"]
+    df_doc.drop(columns=["% de cellules tumorales", "% de cellules à analyser"], inplace=True)
+
+    # Process the tables
+    df_table = pd.DataFrame()
+    for i,table in enumerate(tables):
+        if table.n < 3: # if there's no table results
+            df = None
+        else:
+            df = table[2].df
+        if df is not None:
+            # Extract the first row and split it by '\n' to get the column names
+            column_names = df.iloc[0, 0].split('\n')
+            df = df.drop(0).reset_index(drop=True)
+            # Split the remaining rows by '\n' and create a new DataFrame
+            df = df[0].str.split('\n', expand=True)
+            df.columns = column_names
+            df["Examen"] = df_doc.loc[i, "EXAMEN"] #add the exam number
+            for _, row in df.iterrows():
+                df_table = df_table._append(row, ignore_index=True)
+
+    # Drop column unused
+    df_table.drop(columns=["muté","% d’ADN "], inplace=True)
+
+    # Handles the impact clinique
+    df_table["Impact clinique"] = None
+    impact_clinique = None
+    for index, row in df_table.iterrows():
+        if row["Gène "] == "Mutations avec impact clinique potentiel":
+            impact_clinique = "potentiel"
+            delete_row = True
+        elif row["Gène "] == "Mutations avec impact clinique indéterminé":
+            impact_clinique = "indéterminé"
+            delete_row = True
+        elif row["Gène "] == "Mutations avec impact clinique avéré":
+            impact_clinique = "avéré"
+            delete_row = True
+        else:
+            df_table.at[index, "Impact clinique"] = impact_clinique
+            delete_row = False
+        if delete_row:
+            df_table.drop(index, inplace=True)
+            delete_row = False
+
+    # Reorder the columns of df_table
+    columns = ["Examen"] + [col for col in df_table.columns if col != "Examen"]
+    df_table = df_table[columns]
+
+    logging.info("Data post-processed")
+    return df_doc, df_table
 
 if __name__ == "__main__":
     # Find all PDF files in the pdf folder and save them in a list
     pdf_folder_path = "data/PDF"
     save_folder_path = "data/processed_parser"
     pdf_files = [f for f in os.listdir(pdf_folder_path) if f.endswith('.pdf')]
+    # pdf_files = ["24EM03352.pdf"]
 
-    all_data = []
+    list_data = []
+    table_list = []
     for file in pdf_files:
         pdf_path = f"{pdf_folder_path}/{file}"
-        data = extract_with_pymupdf(pdf_path)
-        table_list = extract_tables_camelot(pdf_path, file, save_folder_path)
-        if data:
-            all_data.append(data)
-
-    df = pd.DataFrame(all_data)
+        text = extract_with_pdfplumber(pdf_path)
+        metadata = format_metadata(text)
+        if metadata:
+            list_data.append(metadata)
+        table_list.append(extract_tables_camelot(pdf_path, file, save_folder_path))
+    
+    df_doc, df_tables = post_process_data(list_data, table_list)
 
     # Save the dataframe to a csv file
-    df.to_csv(f"{save_folder_path}/extracted_pdfparser.csv", index=False)
+    df_doc.to_excel(f"{save_folder_path}/extracted_metadata.xlsx", index=False)
+    df_tables.to_excel(f"{save_folder_path}/extracted_results_mutation.xlsx", index=False)
