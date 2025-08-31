b18eb7332c2ea4e8afac4116156946a5e091d9c7
ACHTEN Alexandre <aachten@work061.cmmi.h6n.ulb.ac.be>
2025-08-31 22:23:22 +0200

    Finish experiments with visuals on extended data

--- src/LLM_pipeline.py
+++ src/LLM_pipeline.py
@@ -30,12 +30,14 @@ pdf_image = {} # used for images in pdfs, not used here
 for pdf_file in pdf_files_path:
     pdf_texts[os.path.splitext(pdf_file)[0]] = extract_with_pdfplumber(os.path.join(pdf_folder_path,pdf_file))
     pdf_image[os.path.splitext(pdf_file)[0]] = extract_pdf2image(os.path.join(pdf_folder_path,pdf_file))
+logger.info(f"Finish processing {len(pdf_texts)} PDFs")
 
 # Load system prompts
 with open("prompt/final_prompt_metadata_final.md", "r") as f:
     system_prompt_meta = f.read()
 with open("prompt/final_prompt_mutation_final.md", "r") as f:
     system_prompt_mut = f.read()
+logger.info("System prompts loaded")
 
 # Login to Hugging Face to enable the use of gemma 3
 with open("login_huggingface.txt", "r") as f:
@@ -47,14 +49,14 @@ try:
 except Exception as e:
     logger.error(f"Failed to login to hugging face: {e}")
 
-model_name = "google/gemma-3-27b-it"
-model_name_shrt = "gemma3_27B_clp_ost_chp_ex" #used for output files
+model_name = "google/gemma-3-4b-it"
+model_name_shrt = "gemma3_4BV_clp_ost_chp_ex" #used for output files
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 logger.info(f"Using device: {device}")
 try:
     pipe = pipeline(
-        "text-generation", #"text-generation" or "image-text-to-text"
+        "image-text-to-text", #"text-generation" or "image-text-to-text"
         model=model_name,
         torch_dtype=torch.bfloat16,
         #device=device,
@@ -71,7 +73,7 @@ metadata_data = []
 mutation_data = []
 # loop to process multiple pdfs
 for pdf_number,text_pdf in tqdm(pdf_texts.items()):
-    logger.info(f"Processing PDF: {pdf_number}")
+    logger.debug(f"Processing PDF: {pdf_number}")
     messages_meta = [
         {
             "role": "system",
@@ -81,7 +83,7 @@ for pdf_number,text_pdf in tqdm(pdf_texts.items()):
             "role": "user",
             "content": (
                 [{"type": "text", "text": text_pdf}]
-                #+ [{"type": "image", "image": img} for img in pdf_image[pdf_number]]
+                + [{"type": "image", "image": img} for img in pdf_image[pdf_number]]
             )
         }
     ]
@@ -95,20 +97,20 @@ for pdf_number,text_pdf in tqdm(pdf_texts.items()):
             "role": "user",
             "content": (
                 [{"type": "text", "text": text_pdf}]
-                #+ [{"type": "image", "image": img} for img in pdf_image[pdf_number]]
+                + [{"type": "image", "image": img} for img in pdf_image[pdf_number]]
             )
         }
     ]
 
     # Run the inference
     start_time = time.time()
-    output_meta = pipe(messages_meta, max_new_tokens=400) # if in image-text-to-text mode, must precise text= parameter
+    output_meta = pipe(text=messages_meta, max_new_tokens=400) # if in image-text-to-text mode, must precise "text=" parameter
     elapsed_time = time.time() - start_time
     time_meta_data.append(elapsed_time)
-    logger.info(f"Pipeline inference time for metadata: {elapsed_time:.2f} seconds")
+    logger.info(f"\nPipeline inference time for metadata: {elapsed_time:.2f} seconds")
 
     start_time = time.time()
-    output_mut = pipe(messages_mut, max_new_tokens=1500)
+    output_mut = pipe(text=messages_mut, max_new_tokens=1500)
     elapsed_time = time.time() - start_time
     time_mutation_data.append(elapsed_time)
     logger.info(f"Pipeline inference time for mutations: {elapsed_time:.2f} seconds")
